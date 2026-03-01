ce16fe4127de00bf12c2a6819b64a7725dfbdf7e
Shreyas <4975shreyasy@gmail.com>
2026-03-01 06:32:59 +0530

    FINAL PUSH EVERYTHING WORKS

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
             <AuthForm view="login" />
 
             {/* Divider */}
-            <div className="relative my-6">
+            {/* <div className="relative my-6">
               <div className="absolute inset-0 flex items-center">
                 <span className="w-full border-t border-border" />
               </div>
@@ -72,11 +72,10 @@ export default function LoginPage() {
                   Or continue with
                 </span>
               </div>
-            </div>
+            </div> */}
 
             {/* OAuth Buttons */}
-            <div className="flex flex-row gap-x-2 justify-center">
-              {/* Google */}
+            {/* <div className="flex flex-row gap-x-2 justify-center">
               <button
                 onClick={() => handleOAuthLogin("google")}
                 className="w-full flex items-center justify-center gap-3 rounded-lg border border-border bg-background hover:bg-muted px-4 py-4 text-sm font-medium transition"
@@ -86,10 +85,8 @@ export default function LoginPage() {
                   alt="Google"
                   className="w-5 h-5"
                 />
-                {/* Login with Google */}
               </button>
 
-              {/* Facebook */}
               <button
                 onClick={() => handleOAuthLogin("facebook")}
                 className="w-full flex items-center justify-center gap-3 rounded-lg border border-border bg-background hover:bg-muted px-4 py-4 text-sm font-medium transition"
@@ -99,7 +96,6 @@ export default function LoginPage() {
                   alt="Facebook"
                   className="w-5 h-5"
                 />
-                {/* Login with Facebook */}
               </button>
 
               <button
@@ -122,11 +118,9 @@ export default function LoginPage() {
                     <path d="M18.71 19.5C17.88 20.74 17 21.95 15.66 21.97C14.32 22 13.89 21.18 12.37 21.18C10.84 21.18 10.37 21.95 9.09997 22C7.78997 22.05 6.79997 20.68 5.95997 19.47C4.24997 17 2.93997 12.45 4.69997 9.39C5.56997 7.87 7.12997 6.91 8.81997 6.88C10.1 6.86 11.32 7.75 12.11 7.75C12.89 7.75 14.37 6.68 15.92 6.84C16.57 6.87 18.39 7.1 19.56 8.82C19.47 8.88 17.39 10.1 17.41 12.63C17.44 15.65 20.06 16.66 20.09 16.67C20.06 16.74 19.67 18.11 18.71 19.5ZM13 3.5C13.73 2.67 14.94 2.04 15.94 2C16.07 3.17 15.6 4.35 14.9 5.19C14.21 6.04 13.07 6.7 11.95 6.61C11.8 5.46 12.36 4.26 13 3.5Z"></path>{" "}
                   </g>
                 </svg>
-                {/* Login with Apple */}
               </button>
-            </div>
+            </div> */}
 
-            {/* Register */}
             <div className="mt-6 text-center text-sm text-muted-foreground">
               Don't have an account?{" "}
               <Link
